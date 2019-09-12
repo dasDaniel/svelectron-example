3669ce7b3bdf9ac931099fe621e6083609ddb0c6
dasDaniel <dasDaniel@users.noreply.github.com>
2019-09-12 08:58:39 -0600

    Update rollup.config.js
    fix dest directory for js and css

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'app/view/bundle.js'
+		file: 'public/bundle.js'
 	},
 	plugins: [
 		svelte({
@@ -21,7 +21,7 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
-				css.write('app/view/bundle.css');
+				css.write('public/bundle.css');
 			}
 		}),
 
